79945ad96eee30074842aae6d48c30709423edd7
Amanda Giacometti <amandagiacomettipedrosa@gmail.com>
2022-11-11 06:16:49 -0300

    iniciando o projeto

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function HomePage() {
+    const estiloDaHomePage = {backgroundColor:"red"}
+    return (
+    <div style={estiloDaHomePage}> Olá </div>
+    
+    
+    
+    
+    )
+}
+
+export default HomePage
